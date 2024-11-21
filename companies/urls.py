0bdc887a17f13e87bdf95c01339617dd2180e1f8
Pedro Henrique PHD <danesvara2002@gmail.com>
2024-11-21 17:47:36 -0300

    feat: add permissions route and view

--- companies/urls.py
+++ companies/urls.py
@@ -1,8 +1,11 @@
 from django.urls.resolvers import URLPattern
 from companies.views.employees import Employees, EmployeeDetail
+from companies.views.permissions import PermissionDetail
 from django.urls import path
 
 urlpatterns: list[URLPattern] = [
     path("employees", Employees.as_view()),
     path("employees/<int:employee_id>", EmployeeDetail.as_view()),
+    
+    path("permissions", PermissionDetail.as_view()),
 ]
